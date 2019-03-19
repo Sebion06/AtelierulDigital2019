c445cd34b13d682b3beeb09ffadddf36bf85f316
sebion06 <sebastianmoticica@gmail.com>
2019-03-20 01:17:19 +0200

    Added challenges

---
+++ src/Challenges/FooBarQix.java
@@ -0,0 +1,53 @@
+package Challenges;
+
+import java.util.Scanner;
+
+public class FooBarQix {
+    public static void compute(int n) {
+        int i = 1, j, k = 0;
+        while (i <= n) {
+            System.out.print(i + " = ");
+            j = i;
+            k = 0;
+            while (j > 0) {
+                if (j % 10 == 3) {
+                    System.out.print("Foo");
+                    k = 1;
+                } else {
+                    if (j % 10 == 5) {
+                        System.out.print("Bar");
+                        k = 1;
+                    } else {
+                        if (j % 10 == 7) {
+                            System.out.print("Qix");
+                            k = 1;
+                        }
+                    }
+                }
+                j = j / 10;
+            }
+            if (i % 3 == 0) {
+                System.out.print("Foo");
+                k = 1;
+            }
+            if (i % 5 == 0) {
+                System.out.print("Bar");
+                k = 1;
+            }
+            if (i % 7 == 0) {
+                System.out.print("Qix");
+                k = 1;
+            }
+            if (k == 0)
+                System.out.print(i);
+            System.out.println();
+            i++;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        compute(n);
+    }
+}
---
+++ src/Challenges/pair_of_2.java
@@ -0,0 +1,23 @@
+package Challenges;
+
+import java.util.Scanner;
+
+public class pair_of_2 {
+    public static void pairs(int v[]) {
+        int k=0;
+        for (int i=0;i<10-1;i++) {
+            for (int j = i + 1; j < 10; j++)
+                if (v[i] + v[j] == 0)
+                    k++;
+        }
+        System.out.println("k = "+k);
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int array[] = new int[10];
+        for (int i = 0; i < 10; i++)
+            array[i] = input.nextInt();
+        pairs(array);
+    }
+}
