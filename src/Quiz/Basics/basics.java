2aa04f642a91f4d17a31036e9d57cce232997e10
sebion06 <sebastianmoticica@gmail.com>
2019-05-06 20:00:23 +0300

    lab8

---
+++ src/Quiz/Basics/basics.java
@@ -0,0 +1,10 @@
+package Quiz.Basics;
+
+public class basics {
+    public static void main(String[] args) {
+        int i=1;
+        do{
+            System.out.println(i++);
+        }while (i!=5);
+    }
+}
